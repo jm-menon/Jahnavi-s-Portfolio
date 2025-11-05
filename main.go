94b4debe16787d6ce4470956cd6cfb653a07e857
Jahnavi <work.jahnavimenon@gmail.com>
2025-11-05 23:43:06 +0530

    added other page links to the main page

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+
+	//fs:= http.FileServer(http.Dir("pages"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/":
+			http.ServeFile(w, r, "pages/index.html")
+		case "/about":
+			http.ServeFile(w, r, "pages/about.html")
+		case "/blog":
+			http.ServeFile(w, r, "pages/blogs.html")
+		case "/projects":
+			http.ServeFile(w, r, "pages/projects.html")
+		default:
+			http.NotFound(w, r)
+		}
+
+	})
+	log.Println("Server starting on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
